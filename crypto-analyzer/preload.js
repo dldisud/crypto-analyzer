b43706a57f2013cc4c7c70979e2262d465bba7b0
dldisud <87322286+dldisud@users.noreply.github.com>
2025-07-22 09:52:20 +0900

    Delete crypto-analyzer directory

--- crypto-analyzer/preload.js
+++
@@ -1,3 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-// We can selectively expose APIs to the renderer process here if needed.
